03278a39ecf7d25e069882ab3779abb73f1403a7
Oween <34008762+cogrpar@users.noreply.github.com>
2020-02-03 08:27:47 -0700

    Update qcpuWare.py

--- QCPU_Setup/solver/qcpuWare.py
+++ QCPU_Setup/solver/qcpuWare.py
@@ -4,7 +4,7 @@ from solveBQM import *
 from solveMatrix import *
 
 #function to define the variables given the number of vars
-def defVars(numOfVars):
+def DefVars(numOfVars):
   vars = []
   for i in range(numOfVars):
     vars.append("v" + str(i))
@@ -19,16 +19,16 @@ for i in var: #this defines each variable using sympy's symbols function
   vars()[i] = symbols(i)
 
 eq = v1 + v2 - v0 - v3 #really we would get this equation from webserver
-EQ, numOfVars, places = toBin(domain, eq, var) #convert function to binary and get it as a string, along with a new variable count and an array used to convert back to decimal
+EQ, numOfVars, places = ToBin(domain, eq, var) #convert function to binary and get it as a string, along with a new variable count and an array used to convert back to decimal
 
 #now solve for the matrix:
-inVars = defVars(numOfVars)
-matrix = solvMatrix(EQ, inVars, False) #set to true to maximise result. Really we would get this boolean from webserver
+inVars = DefVars(numOfVars)
+matrix = SolvMatrix(EQ, inVars, False) #set to true to maximise result. Really we would get this boolean from webserver
 
 #send to dwave to solve for max/min:
-exec(setMatrix(matrix, numOfVars))
-vals = solveExtreme(bqm)
+exec(SetMatrix(matrix, numOfVars))
+vals = SolveExtreme(bqm)
 
 #convert the results back to decimal and send them to the webserver:
-results = toDec(places, vals)
+results = ToDec(places, vals)
 print(results)
