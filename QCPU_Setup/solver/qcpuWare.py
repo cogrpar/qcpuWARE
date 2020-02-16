ed8e4786bceab8ddd32e3c3f6b52913b805f0074
Oween <34008762+cogrpar@users.noreply.github.com>
2020-02-16 09:58:40 -0600

    Update qcpuWare.py

--- QCPU_Setup/solver/qcpuWare.py
+++ QCPU_Setup/solver/qcpuWare.py
@@ -3,6 +3,7 @@ from baseConverter import *
 from solveBQM import *
 from solveMatrix import *
 from sympy import *
+from time import sleep
 
 #function to define the variables given the number of vars
 def DefVars(numOfVars):
@@ -42,9 +43,8 @@ def GetInput(): #function to take the input from the server file
   inp = open("/var/www/html/storage.txt","w+")
   inp.write("")
   inp.close()
-    
-  Return = [dom, eq, minMaxBool] #add all components to an array and return it
-  return (Return)
+  
+  return (dom, eq, minMaxBool)
 
 #function that checks to see if the storage file is empty
 def EmptyInput():
@@ -57,29 +57,35 @@ def EmptyInput():
   else:
     return (False)
 
-#code
-domain = [4, 4, 4, 4] #really we would get this array from webserver
-numOfVars = len(domain)
+####################  code  ####################
+while True: #run repeatedly in the background
+  #check to see if there is any input on the server
+  if (not EmptyInput()): 
+    domain, eq, minMax = GetInput()
+    numOfVars = len(domain)
 
-#generate array containing variables:
-var = []
-for i in range(numOfVars):
-  var.append("v" + str(i))
-  
-for i in var: #this defines each variable using sympy's symbols function
-  vars()[i] = symbols(i)
+    #generate array containing variables:
+    var = []
+    for i in range(numOfVars):
+      var.append("v" + str(i))
 
-eq = v1 + v2 - v0 - v3 #really we would get this equation from webserver
-EQ, numOfVars, places = ToBin(domain, eq, var) #convert function to binary and get it as a string, along with a new variable count and an array used to convert back to decimal
+    for i in var: #this defines each variable using sympy's symbols function
+      vars()[i] = symbols(i)
 
-#now solve for the matrix:
-inVars = DefVars(numOfVars)
-matrix = SolvMatrix(EQ, inVars, False) #set to true to maximise result. Really we would get this boolean from webserver
+    EQ, numOfVars, places = ToBin(domain, eq, var) #convert function to binary and get it as a string, along with a new variable count and an array used to convert back to decimal
 
-#send to dwave to solve for max/min:
-exec(SetMatrix(matrix, numOfVars))
-vals = SolveExtreme(bqm)
+    #now solve for the matrix:
+    inVars = DefVars(numOfVars)
+    matrix = SolvMatrix(EQ, inVars, nimMax) #set to true to maximise result. Really we would get this boolean from webserver
 
-#convert the results back to decimal and send them to the webserver:
-results = ToDec(places, vals)
-print(results)
+    #send to dwave to solve for max/min:
+    exec(SetMatrix(matrix, numOfVars))
+    vals = SolveExtreme(bqm)
+
+    #convert the results back to decimal and send them to the webserver:
+    results = ToDec(places, vals)
+    #print(results)
+    ReturnResults(results)
+    
+  else: #if there is no input, then wait a bit and check again
+    sleep(0.3)
