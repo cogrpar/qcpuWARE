3ab60fba1688b393d15df32fa56cec52384b277d
Oween <34008762+cogrpar@users.noreply.github.com>
2020-02-18 15:39:01 -0700

    Update setup.py

--- QCPU_Setup/setup.py
+++ QCPU_Setup/setup.py
@@ -89,6 +89,7 @@ sym.wait()
 
 #append command to start code on boot
 onBoot = open("/etc/crontab", "a+")
+prg1 = os.path.dirname(os.path.abspath(__file__))).replace("setup.py", "solver/qcpuWare.py") #get the location of the solver file
 progrms = [] #put the name of each program in this list to write them to the contrab file so they run on boot...
 for progrm in progrms:
     onBoot.write("@reboot " + progrm)
