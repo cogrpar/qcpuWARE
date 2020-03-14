029ee545469b1bd4a13cf2c5cd6758edf926b1ca
Oween <34008762+cogrpar@users.noreply.github.com>
2020-03-14 14:13:17 -0500

    Create setup.java

---
+++ JavaLibrary/setup.java
@@ -0,0 +1,21 @@
+class setup{
+  public static void main (String [] args){
+    //this is the instalation code for the java library
+    
+    //if args[0] is install:
+    if(args[0] == "install"){
+    
+    }
+    
+    //else if args[0] is update
+    else if(args[0] == "update"){
+    
+    }
+    
+    //else
+    else{
+      System.out.println("Invalid arg: " + arg[0]);
+    }
+    
+  }
+}
