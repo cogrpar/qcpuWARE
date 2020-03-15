e1c9cdb8e1f78a9e5a6d1736fb11e208b8134265
Oween <34008762+cogrpar@users.noreply.github.com>
2020-03-15 18:34:07 -0500

    Update setup.java
    added function to handle install and update args

--- JavaLibrary/setup.java
+++ JavaLibrary/setup.java
@@ -1,15 +1,22 @@
 class setup{
   public static void main (String [] args){
-    //this is the instalation code for the java library
+    //this is the instalation code for the java library (execute as root)
     
     //if args[0] is install:
     if(args[0] == "install"){
-    
+      //execute command to add the software to classpath
+      try{
+        String workDir = System.getProperty("user.dir");
+        Runtime.getRuntime().exec("javac -classpath .:" + workDir + " qcpuWare.java");
+      }catch(Exception e){}
     }
     
     //else if args[0] is update
     else if(args[0] == "update"){
-    
+      //git pull
+      try{
+        Runtime.getRuntime().exec("git pull");
+      }catch(Exception e){}
     }
     
     //else
