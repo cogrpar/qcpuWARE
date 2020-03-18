4af3b3f82ecdbe9f7ad90665c2d9ee870832043e
Oween <34008762+cogrpar@users.noreply.github.com>
2020-03-18 13:50:40 -0500

    Update setup.java

--- JavaLibrary/setup.java
+++ JavaLibrary/setup.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 class setup{
   public static void main (String [] args){
     //this is the instalation code for the java library (execute as root)
@@ -5,10 +7,7 @@ class setup{
     //else if args[0] is update
     if(args[0].contains("update")){
       //git pull
-      System.out.println("updating...");
-      try{
-        Runtime.getRuntime().exec("git reset --hard && git pull");
-      }catch(Exception e){}
+      System.out.println("To update, please run 'git update' in the directory containing this update file.");
     }
     
     //else
