add8bad21d3c435566fbee3546de361211d63daa
Oween <34008762+cogrpar@users.noreply.github.com>
2020-03-21 16:12:49 -0500

    Update solveBQM.py
    Removed unnecessary function to define BQM now that we don't need to use matrices directly.
    Added function that solves for BQM in CSP mode.

--- QCPU_Setup/solver/solveBQM.py
+++ QCPU_Setup/solver/solveBQM.py
@@ -26,32 +26,37 @@ def SolveExtreme(bqm):
   results = result.split(", ") #separate the vars
 
   #now extract the numerical results:
+  refined = []
   for i in range(len(results)):
-    results[i] = float(results[i].replace((str(i+1) + ": "), ""))
+    if (not("*" in results[i])): #if this is a useful result that only contains one term 
+      refined.append(float(results[i].replace((str(i+1) + ": "), "")))
+  return(refined)
+
+
+#function to solve BQM formatted from a CSP
+def SolveCSP(bqm):
+  # Define the workflow
+  iteration = hybrid.RacingBranches(
+      hybrid.InterruptableTabuSampler(),
+      hybrid.EnergyImpactDecomposer(size=2)
+      | hybrid.QPUSubproblemAutoEmbeddingSampler()
+      | hybrid.SplatComposer()
+  ) | hybrid.ArgMin()
+  workflow = hybrid.LoopUntilNoImprovement(iteration, convergence=3)
+
+  # Solve the problem
+  init_state = hybrid.State.from_problem(bqm)
+  final_state = workflow.run(init_state).result()
+
+  # Print results
+  result = ("Solution: sample={.samples.first}".format(final_state))
+  result = result.replace("Solution: sample=Sample(sample={", "")
+  result, trash = result.split("}")
+  results = result.split(", ")
+
+  #now extract the numerical results:
+  for i in range(len(results)):
+    trash, term = results[i].split(": ")
+    results[i] = float(term)
   return(results)
   
-
-#function to set the values of the bqm matrix for the dwave system to minimize/maximize
-def SetMatrix(matrix, numOfVars):
-  equal = []
-  dif = []
-  for i in range(numOfVars):
-    for j in range(numOfVars):
-      if (i == j):
-        if (i < numOfVars-1): #if we are not doing the last entry
-          equal.append(str(i+1) + ": " + str(matrix[i][j]) + ", ")
-        else:
-          equal.append(str(i+1) + ": " + str(matrix[i][j]))
-      if (i < j):
-        dif.append("(" + str(i+1) + ", " + str(j+1) + "): " + str(matrix[i][j]) + ", ")
-  #now combine all of these into a single executable function:
-  exe = "bqm = dimod.BinaryQuadraticModel({"
-  for i in equal:
-    exe += i
-  exe += "}, {"
-  for i in dif:
-    exe += i
-  exe = exe[:-2:]
-  exe += "}, 0.0, dimod.BINARY)"
-  
-  return(exe)
